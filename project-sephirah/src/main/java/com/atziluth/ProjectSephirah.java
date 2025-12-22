6d64fddb700dd90d1b2f2d9ae07cc3487daff6fc
ProfessorCroquette <shiro.argentum@gmail.com>
2025-12-23 00:02:38 +0700

    Umapyoi!

--- project-sephirah/src/main/java/com/atziluth/ProjectSephirah.java
+++ project-sephirah/src/main/java/com/atziluth/ProjectSephirah.java
@@ -100,7 +100,6 @@ public class ProjectSephirah {
         }
     }
     
-    // ...existing code...
     private void showHelp() {
         System.out.println("""
             📖 PROJECT SEPHIRAH HELP
